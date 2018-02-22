0b3830973a99e1560556087f391d19ccf99b3566
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-02-22 03:28:02 +0300

    1 test failed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ module.exports = function check(str, bracketsConfig) {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
       if (str[i] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]){
-        
+        for (let z = 0; z < i; z++){
+          if (str[z] == bracketsConfig[j][0]) {
+            k += 1;
+          }
+        }
+        if (k == 0) {
+          brackets.push(str[i]);
+        } else if (k == 1) {
+          let temp = brackets.pop();
+          if (temp != bracketsConfig[j][0]) {
+            return false;
+          }
+        }
       } else {
         if (str[i] == bracketsConfig[j][0]) {
           brackets.push(str[i]);
@@ -20,9 +32,10 @@ module.exports = function check(str, bracketsConfig) {
             return false;
           }
         }
-      }
+      }   
     }
   }
+
   if (brackets.length != 0){
     return false;
   }
