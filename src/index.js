6fd5fe97bc737bd706fee53ca74a0d268eea3ee5
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-02-26 19:50:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,43 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
   let n = str.length;
-  let check_arr = {};
   let brackets = [];
   let k = 0;
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
-      if (str[i] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]){
-        for (let z = 0; z < i; z++){
-          if (str[z] == bracketsConfig[j][0]) {
-            k += 1;
-          }
-        }
-        if (k == 0) {
-          brackets.push(str[i]);
-        } else if (k == 1) {
-          let temp = brackets.pop();
-          if (temp != bracketsConfig[j][0]) {
-            return false;
-          }
-        }
-      } else {
-        if (str[i] == bracketsConfig[j][0]) {
-          brackets.push(str[i]);
-        }
-        if (str[i] == bracketsConfig[j][1]) {
-          if (brackets == []) {
-            return false;
-          }
-          let temp = brackets.pop();
-          if (temp != bracketsConfig[j][0]) {
-            return false;
-          }
-        }
-      }   
+      if (str[i] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1] && brackets[brackets.length - 1] == bracketsConfig[j][0]) {
+        k--;
+        brackets.pop();
+      } else if (str[i] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]) {
+        k++;
+        brackets.push(str[i]);
+      } else if (str[i] == bracketsConfig[j][1] && k == 0) {
+        return false;
+      } else if (str[i] == bracketsConfig[j][1] && brackets[brackets.length - 1] != bracketsConfig[j][0]) {
+        return false;
+      } else if (str[i] == bracketsConfig[j][1] && brackets[brackets.length - 1] == bracketsConfig[j][0]) {
+        k--;
+        brackets.pop();
+      }
+      if (str[i] == bracketsConfig[j][0] && str[i] != bracketsConfig[j][1]) {
+        k++;
+        brackets.push(str[i]);
+      }
     }
   }
-
   if (brackets.length != 0){
     return false;
   }
-  return true; 
+  if (k != 0){
+    return false;
+  }
+  return true;
 }
